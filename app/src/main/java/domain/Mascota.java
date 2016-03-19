99139771bf61b4f7ab02c1e5542da10e0ce6057a
yessenia pc <jessiejorge@gmail.com>
2016-03-18 22:49:47 -0500

    v 2.2 mascota

--- app/src/main/java/domain/Mascota.java
+++ app/src/main/java/domain/Mascota.java
@@ -5,16 +5,69 @@ package domain;
  */
 public class Mascota {
 
+    private int codigo_mascota;
     private int imagen;
     private String nombre;
     private String edad;
+    private String fecha_nacimiento;
+    private String sexo;
+    private String raza;
+    private String peso;
+    private String alimentacion;
+    private String tratamiento;
 
-    public Mascota(String nombre, String edad, int imagenPerrito) {
+    public Mascota(int codigo_mascota, int imagen, String nombre, String edad, String fecha_nacimiento, String sexo, String raza, String peso, String alimentacion, String tratamiento) {
+        this.codigo_mascota = codigo_mascota;
+        this.imagen = imagen;
         this.nombre = nombre;
-        this.edad   = edad;
-        this.imagen = imagenPerrito;
+        this.edad = edad;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.sexo = sexo;
+        this.raza = raza;
+        this.peso = peso;
+        this.alimentacion = alimentacion;
+        this.tratamiento = tratamiento;
     }
 
+    public int getCodigo_mascota() {
+        return codigo_mascota;
+    }
+
+    public void setCodigo_mascota(int codigo_mascota) {
+        this.codigo_mascota = codigo_mascota;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
 
     public int getImagen() {
         return imagen;
@@ -39,4 +92,22 @@ public class Mascota {
     public void setEdad(String edad) {
         this.edad = edad;
     }
+
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
 }
