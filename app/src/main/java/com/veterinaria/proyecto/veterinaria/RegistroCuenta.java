4e9af31d3be5549bb8eef9035d623561c59a68fc
nosoyungato <steph.callegari@icloud.com>
2016-03-19 19:16:21 -0500

    RegistroCuenta
    Agregué Activity de RegistroCuenta

---
+++ app/src/main/java/com/veterinaria/proyecto/veterinaria/RegistroCuenta.java
@@ -0,0 +1,21 @@
+package com.veterinaria.proyecto.veterinaria;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class RegistroCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_registro_cuenta);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+}
