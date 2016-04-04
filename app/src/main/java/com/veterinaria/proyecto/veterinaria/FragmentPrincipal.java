679bc9259a95d34fd54f5f5b6b700de4a30b6c99
cmoreno <cmoreno0309@gmail.com>
2016-04-03 23:35:18 -0500

    CMG:

--- app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPrincipal.java
+++ app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPrincipal.java
@@ -2,10 +2,13 @@ package com.veterinaria.proyecto.veterinaria;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,17 +25,21 @@ public class FragmentPrincipal extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setHasOptionsMenu(true);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.content_principal, container, false);
-
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setTitle("");
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_principal, menu);
     }
 
 
