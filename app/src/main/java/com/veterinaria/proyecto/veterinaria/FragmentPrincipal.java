878e2537e61f9419f9d40d49d127369feae906a4
cmoreno <cmoreno0309@gmail.com>
2016-04-19 14:06:44 -0500

    CMG:

--- app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPrincipal.java
+++ app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPrincipal.java
@@ -13,14 +13,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
 
 /**
  * Created by Cmoreno on 25/03/2016.
  */
 public class FragmentPrincipal extends Fragment {
 
-    private FragmentTabHost mTabHost;
 
     public FragmentPrincipal(){
 
@@ -29,35 +27,15 @@ public class FragmentPrincipal extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setHasOptionsMenu(true);
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.content_principal, container, false);
-        /*AppCompatActivity activity = (AppCompatActivity) getActivity();
-        activity.getSupportActionBar().setTitle("");*/
-        Resources res = getResources();
-       // View rootView = inflater.inflate(R.layout.content_principal, container, false);
-        TabHost tabs = (TabHost) rootView.findViewById(android.R.id.tabhost);
-        tabs.setup();
-
-        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("", res.getDrawable(android.R.drawable.ic_btn_speak_now));
-        tabs.addTab(spec);
-
-        spec=tabs.newTabSpec("mitab2");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("TAB2", res.getDrawable(android.R.drawable.ic_dialog_map));
-        tabs.addTab(spec);
-
-        //tabs.addTab(mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),FragmentMascota.class, null);
-
-        tabs.setCurrentTab(0);
-
-
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setTitle("");
         return rootView;
     }
 
