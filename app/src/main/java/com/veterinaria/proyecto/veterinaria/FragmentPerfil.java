f132d4485a3d45c38ec18fbd1c518605cf7f9311
Stephanie <steph.callegari@icloud.com>
2016-04-21 15:37:13 -0500

    FragmentPerfil
    Perfil (el usuario dueño podrá ver su perfil si ha hecho login)

---
+++ app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPerfil.java
@@ -0,0 +1,21 @@
+package com.veterinaria.proyecto.veterinaria;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sCallegari on 21/04/16.
+ */
+public class FragmentPerfil extends Fragment{
+    public FragmentPerfil(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
