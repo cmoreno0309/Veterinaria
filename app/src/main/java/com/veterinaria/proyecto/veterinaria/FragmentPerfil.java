93d196b423297ea4b7c1fc01a11d821e02efff9a
Stephanie <steph.callegari@icloud.com>
2016-04-23 14:54:49 -0500

    Added files via upload

--- app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPerfil.java
+++ app/src/main/java/com/veterinaria/proyecto/veterinaria/FragmentPerfil.java
@@ -2,6 +2,9 @@ package com.veterinaria.proyecto.veterinaria;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by sCallegari on 21/04/16.
@@ -16,6 +19,12 @@ public class FragmentPerfil extends Fragment{
         super.onCreate(savedInstanceState);
 
 
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootview = inflater.inflate(R.layout.content_dueno, container, false);
+
+        return rootview;
     }
 
 }
